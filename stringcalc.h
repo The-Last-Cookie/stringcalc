8bb977b131f1c8a09c41f8110b46750e99407763
The-Last-Cookie <koller.jonas@online.de>
2020-11-14 19:14:47 +0100

    add comments for documentation

--- stringcalc.h
+++ stringcalc.h
@@ -6,14 +6,20 @@
 
 namespace StringCalc {
 
+	// Addition
 	std::string add(std::string str1, std::string str2);
 
+	// Sutraction
 	std::string sub(std::string str1, std::string str2);
 
+	// Multiplication
 	std::string mult(std::string str1, std::string str2);
 
+	// Division (without remainder)
 	std::string div(std::string str1, std::string str2);
 
+	// Return string with higher value
+	// If both strings are the same, the function will return an empty string
 	std::string max(std::string str1, std::string str2);
 
 }
