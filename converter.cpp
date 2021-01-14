7cd1bd3e0d79842eaa09b9f213b80877bfc28342
The-Last-Cookie <koller.jonas@online.de>
2021-01-14 15:43:45 +0100

    implement convertTo function

--- converter.cpp
+++ converter.cpp
@@ -33,5 +33,42 @@ void Converter::parse(std::string str, unsigned int base) {
 }
 
 std::string Converter::convertTo(unsigned int base) {
-	return "";
+
+	// Input sanitization
+	if (base < 2 || base > 36) {
+		std::cout << "Error: Only the bases from 2 to 36 are allowed!";
+		return "";
+	}
+	else if (base == currentBase) {
+		return value;
+	}
+
+	std::string result = "";
+	std::string decimal = "0";
+
+	// Calculate decimal
+	for (uint64 i = 0; i < value.length(); i++) {
+		std::string index = std::to_string(value.length() - i - 1);
+		std::string coefficient = std::to_string(charToInt(value[i]));
+		std::string temp = Decimal::mult(coefficient, Decimal::pow(std::to_string(currentBase), index));
+		decimal = Decimal::add(decimal, temp);
+	}
+
+	// Decimal to base
+	for (int i = 0; decimal != "0"; i++) {
+		std::string remainder = Decimal::mod(decimal, std::to_string(base));
+		decimal = Decimal::div(decimal, std::to_string(base));
+		
+		// 0 to 9
+		if (remainder.length() == 1) {
+			result.insert(0, remainder);
+		}
+		else {
+			// A to Z
+			remainder = StringCalc::intToString(std::atoi(remainder.c_str()));
+			result.insert(0, remainder);
+		}
+	}
+
+	return result;
 }
