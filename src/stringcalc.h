cbd0cab07c18f90c52d7a0165e7877aa74f70e13
The-Last-Cookie <koller.jonas@online.de>
2022-01-08 15:21:27 +0100

    remove binary and hexadecimal namespaces

--- src/stringcalc.h
+++ src/stringcalc.h
@@ -25,98 +25,32 @@ namespace StringCalc {
 
 	bool isInBase(std::string str, unsigned int base);
 
-	namespace Decimal {
+	// Return string with fewer value
+	// If both strings are the same, return an empty string
+	std::string min(std::string str1, std::string str2);
 
-		// Addition
-		std::string add(std::string addend1, std::string addend2);
+	// Return string with higher value
+	// If both strings are the same, return an empty string
+	std::string max(std::string str1, std::string str2);
 
-		// Sutraction
-		std::string sub(std::string minuend, std::string subtrahend);
+	// Addition
+	std::string add(std::string addend1, std::string addend2);
 
-		// Multiplication
-		std::string mult(std::string factor1, std::string factor2);
+	// Sutraction
+	std::string sub(std::string minuend, std::string subtrahend);
 
-		// Division (without remainder)
-		std::string div(std::string dividend, std::string divisor);
+	// Multiplication
+	std::string mult(std::string factor1, std::string factor2);
 
-		// Modulo
-		std::string mod(std::string dividend, std::string divisor);
+	// Division (without remainder)
+	std::string div(std::string dividend, std::string divisor);
 
-		// Exponentiation
-		std::string pow(std::string base, std::string exponent);
+	// Modulo
+	std::string mod(std::string dividend, std::string divisor);
 
-		// Factorial
-		std::string fact(std::string num);
+	// Exponentiation
+	std::string pow(std::string base, std::string exponent);
 
-		// Return string with fewer value
-		// If both strings are the same, return an empty string
-		std::string min(std::string str1, std::string str2);
-
-		// Return string with higher value
-		// If both strings are the same, return an empty string
-		std::string max(std::string str1, std::string str2);
-
-		bool isDecimal(std::string string);
-
-	}
-
-	namespace Binary {
-
-		// Addition
-		std::string add_b(std::string addend1, std::string addend2);
-
-		// Sutraction
-		std::string sub_b(std::string minuend, std::string subtrahend);
-
-		// Multiplication
-		std::string mult_b(std::string factor1, std::string factor2);
-
-		// Division (without remainder)
-		std::string div_b(std::string dividend, std::string divisor);
-
-		// Return string with fewer value
-		// If both strings are the same, return an empty string
-		std::string min_b(std::string str1, std::string str2);
-
-		// Return string with higher value
-		// If both strings are the same, return an empty string
-		std::string max_b(std::string str1, std::string str2);
-
-		bool isBinary(std::string string);
-
-	}
-
-	namespace Hexadecimal {
-
-		// Addition
-		std::string add_h(std::string addend1, std::string addend2);
-
-		// Sutraction
-		std::string sub_h(std::string minuend, std::string subtrahend);
-
-		// Multiplication
-		std::string mult_h(std::string factor1, std::string factor2);
-
-		// Division (without remainder)
-		std::string div_h(std::string dividend, std::string divisor);
-
-		// Return string with fewer value
-		// If both strings are the same, return an empty string
-		std::string min_h(std::string str1, std::string str2);
-
-		// Return string with higher value
-		// If both strings are the same, return an empty string
-		std::string max_h(std::string str1, std::string str2);
-
-		// DEPRECATED
-		// Convert letter to corresponding int
-		int letterToInt(char c);
-
-		// DEPRECATED
-		// Convert int into corresponding letter
-		std::string intToLetter(int num);
-
-		bool isHexadecimal(std::string string);
-
-	}
+	// Factorial
+	std::string fact(std::string num);
 }
