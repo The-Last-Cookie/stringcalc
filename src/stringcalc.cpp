248ff0b358c4274f550927a56d0e476d294db158
The-Last-Cookie <koller.jonas@online.de>
2022-02-22 15:28:52 +0100

    add basic structure for outer functions

--- src/stringcalc.cpp
+++ src/stringcalc.cpp
@@ -4,8 +4,31 @@
 
 #include "stringcalc.h"
 
-std::string StringCalc::add(std::string addend1, std::string addend2) {
-	return "";
+std::string StringCalc::add(unsigned int base, std::string addend1, std::string addend2) {
+	Number x(base, addend1);
+	Number y(base, addend2);
+
+	if (!x.isValid() || !y.isValid()) {
+		return "";
+	}
+
+	// Add zeros, so both strings have the same length
+	if (addend1.length() > addend2.length()) {
+		uint64 zerosToAdd = addend1.length() - addend2.length();
+		y.addZeros(zerosToAdd);
+	}
+	else if (addend1.length() < addend2.length()) {
+		uint64 zerosToAdd = addend2.length() - addend2.length();
+		x.addZeros(zerosToAdd);
+	}
+
+	std::string sum = StringCalc::Helper::h_add(base, x.getValue(), y.getValue());
+
+	// TODO: improve
+	Number n(base, sum);
+	n.removeLeadingZeros();
+	
+	return n.getValue();
 }
 
 std::string StringCalc::sub(std::string minuend, std::string subtrahend) {
